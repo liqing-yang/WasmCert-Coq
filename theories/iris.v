eb7a033fb0dd039a2f5183c3205e9ca6b67059f4
raoxiaojia <raoxiaojia@gmail.com>
2021-04-30 18:00:21 +0100

    implementation using purely stdpp functions, a lot of lookup lemmas

--- theories/iris.v
+++ theories/iris.v
@@ -544,6 +544,26 @@ Inductive loc : Type :=
   | loc_wasm_glob: N -> loc
 .
 
+(* The following two location type definitions are for lookup lemmas later. *)
+Inductive loc_type : Type :=
+  | LOC_HV
+  | LOC_LV
+  | LOC_WF
+  | LOC_WT
+  | LOC_WM
+  | LOC_WG
+.
+
+Definition loc_typeof (l: loc): loc_type :=
+  match l with
+  | loc_host_var _ => LOC_HV
+  | loc_local_var _ => LOC_LV
+  | loc_wasm_func _ => LOC_WF
+  | loc_wasm_tab _ => LOC_WT
+  | loc_wasm_mem _ => LOC_WM
+  | loc_wasm_glob _ => LOC_WG
+  end.
+
 Global Instance loc_eq_decision : EqDecision loc.
 Proof. solve_decision. Defined.
 
@@ -598,11 +618,9 @@ Class heapG Σ := HeapG {
 (*  heapG_proph_mapG :> proph_mapG proph_id (val * val) Σ;*)
 }.
 
+(* Implemented using imap, should be the optimal now *)
 Definition heap_gmap_of_list {T: Type} (l: list T) (f: N -> loc) (g: T -> heap_val) : gmap loc (option heap_val) :=
-  list_extra.fold_lefti
-    (fun (i : nat) (gm : gmap loc (option heap_val)) (x: T) =>
-       map_insert (f (N.of_nat i)) (Some (g x)) gm)
-    l ∅.
+  list_to_map (imap (fun n x => (f (N.of_nat n), Some (g x))) l).
 
 Definition gmap_of_store_func (s : store_record) : gmap loc (option heap_val) :=
   heap_gmap_of_list s.(s_funcs) (fun n => loc_wasm_func n) (fun x => hval_func x).
@@ -625,104 +643,177 @@ Definition gmap_of_store (s : store_record) : gmap loc (option heap_val) :=
 Definition gmap_of_locs (locs: list host_value) : gmap loc (option heap_val) :=
   heap_gmap_of_list locs (fun n => loc_local_var n) (fun x => hval_val x).
 
-(*
-(* 
-  Maybe this already exists in the standard library but I can't find it.
-
-  Given functions f: X -> T1, g: Y -> T2, and a gmap m: X -> Y, construct a new gmap 
-     m': T1 -> T2 that completes the following commutative square:
-
-  X  ---m---> Y
-  |           |
-  f           g
-  ↓           ↓
-  T1 ---m'--> T2
-
-  This is for changing the host variable store hs: id -> val to a component in the heap, which
-    is a gmap h: loc -> heap_val.
-
-  Note that f needs to be injective.
- *)
-
-Print map_imap.
-
-Print Inj.
-
-(* Better implementation using gmap_to_list and list_to_map *)
-Definition f_g_gmap_transform {T1 T2 X Y: Type} {EqDecisionX: EqDecision X} {HX: Countable X} {EqDecisionT: EqDecision T1} {HT: Countable T1} (f: X -> T1) (g: Y -> T2) {Inj0: Inj (=) (=) f} (m: gmap X Y) : gmap T1 T2 :=
-  let lp := map_to_list m in
-  let lp' := fmap (fun p => match p with
-                       | (x, y) => (f x, g y)
-                       end) lp in
-  list_to_map lp'.
-  
-Lemma elem_of_fmap_list {X Y: Type} (f: X -> Y) (l : list X) (y: Y):
-    y ∈ fmap f l <-> ∃ x, x ∈ l /\ f x = y.
-Proof.
-  induction l => //=.
-  - split.
-    + move => H. by inversion H.
-    + move => [x [He _]]. by inversion He.
-  - split.
-    + move => H. Search elem_of.
-Admitted.
-  
-(* I hope there's a way to not have to specify all the implicit types/conditions *)
-Lemma f_g_gmap_transform_spec {T1 T2 X Y: Type} {EqDecisionX: EqDecision X} {HX: Countable X} {EqDecisionT: EqDecision T1} {HT: Countable T1} (f: X -> T1) (g: Y -> T2) {Inj0: Inj (=) (=) f} (m: gmap X Y) (x: X) (y: Y):
-  (f_g_gmap_transform f g m) !! (f x) = option_map g (m !! x).
-Proof.
-  remember ((f_g_gmap_transform f g m) !! (f x)) as res_lookup eqn: Hlookup; symmetry in Hlookup.
-  destruct res_lookup; unfold f_g_gmap_transform, option_map in * => /=.
-  - apply elem_of_list_to_map_2 in Hlookup.
-    apply elem_of_fmap_list in Hlookup. destruct Hlookup as [xy [Hl Hm]].
-    destruct xy; simpl in *. inversion Hm; subst; clear Hm.
-    
-    
-    apply in_map_iff in Hlookup.
-Admitted.
-*)
-  
 Definition heap_gmap_of_hs (hs: host_state) : gmap loc (option heap_val) :=
   list_to_map
   (fmap (fun p => match p with | (x, y) => (loc_host_var x, option_map (fun y => hval_val y) y) end)
         (map_to_list hs)).
 
-Lemma heapg_of_hs_lookup (hs: host_state) (i: id):
-  (heap_gmap_of_hs hs) !! (loc_host_var i) = option_map (option_map (fun y => hval_val y)) (hs !! i).
+Ltac remember_lookup :=
+  match goal with
+  | |- context C [?m !! ?x = _] =>
+    let Hlookup := fresh "Hlookup" in
+    remember (m !! x) as lookup_res eqn: Hlookup; symmetry in Hlookup
+  end.
+
+Ltac resolve_finmap :=
+  repeat match goal with
+         | H: (list_to_map _) !! _ = Some _ |- _ =>
+           let H2 := fresh "H2" in 
+           apply elem_of_list_to_map in H as H2; clear H
+         | H: (list_to_map _) !! _ = None |- _ =>
+           let H2 := fresh "H2" in 
+           apply not_elem_of_list_to_map in H as H2; clear H
+         | H: _ ∈ fmap _ _ |- _ =>
+           let Heq := fresh "Heq" in
+           let Helem := fresh "Helem" in
+           apply elem_of_list_fmap in H; destruct H as [? [Heq Helem]]; subst; simpl in *
+         | H: ?x ∈ map_to_list _ |- _ =>
+           destruct x; apply elem_of_map_to_list in H
+         | H: _ ∈ imap _ _ |- _ =>
+           let Heq := fresh "Heq" in
+           let Helem := fresh "Helem" in
+           apply elem_of_lookup_imap in H; destruct H as [? [? [Heq Helem]]]
+         | H: (_, _) = (_, _) |- _ =>
+           inversion H; subst; clear H
+         | H: _ |- NoDup (fmap fst _) =>
+           apply NoDup_fmap_fst; intros; subst; simpl in *; try by []
+         | H: _ |- NoDup (map_to_list _) =>
+           apply NoDup_map_to_list; try by []
+         | H1: ?m !! ?x = _, H2: ?m !! ?x = _ |- _ =>
+           rewrite H2 in H1; subst; simpl in *; clear H2
+         | H: Some _ = Some _ |- _ =>
+           inversion H; subst; simpl in *; try by []
+         | H: _ |- (_, _) ∈ map_to_list _ =>
+           apply elem_of_map_to_list
+         | _ => simpl in *; try by eauto
+         end.
+
+(* Turns out that this is surprisingly not a standard lemma in stdpp and non-trivial to prove. *)
+Lemma nodup_imap_inj1 {T X: Type} (l: list T) (f: nat -> T -> X):
+  (forall n1 n2 t1 t2, f n1 t1 = f n2 t2 -> n1 = n2) ->
+  NoDup (imap f l).
 Proof.
-  remember ((heap_gmap_of_hs hs) !! (loc_host_var i)) as lookup_res eqn: Hlookup; symmetry in Hlookup.
-  unfold heap_gmap_of_hs, option_map in *.
-  destruct lookup_res.
-  - apply elem_of_list_to_map in Hlookup.
-    + apply elem_of_list_fmap in Hlookup.
-      destruct Hlookup as [[id res] [Heq Helem]].
-      inversion Heq; subst; clear Heq.
-      apply elem_of_map_to_list in Helem. by rewrite Helem.
-    + apply NoDup_fmap_fst.
-      * move => x y1 y2 H1 H2.
-        apply elem_of_list_fmap in H1. destruct H1 as [[x1' y1'] [He1 Helem1]].
-        apply elem_of_list_fmap in H2. destruct H2 as [[x2' y2'] [He2 Helem2]].
-        inversion He1; inversion He2; subst; inversion H3; subst; clear H3.
-        apply elem_of_map_to_list in Helem1.
-        apply elem_of_map_to_list in Helem2.
-        rewrite Helem1 in Helem2. by inversion Helem2.
-      * apply NoDup_fmap; last by apply NoDup_map_to_list.
-        move => p1 p2 Heq.
-        destruct p1 as [x1 y1]. destruct p2 as [x2 y2].
-        inversion Heq; subst.
-        repeat (destruct y1; destruct y2 => //=; try by inversion H2).
-  - apply not_elem_of_list_to_map in Hlookup.
-    rewrite -> elem_of_list_fmap in Hlookup.
-    remember (hs !! i) as lookup_res2 eqn: Hlookup2; symmetry in Hlookup2.
-    repeat destruct lookup_res2 as [lookup_res2|] => //=; exfalso; apply Hlookup; apply elem_of_map_to_list in Hlookup2.
-    + exists (loc_host_var i, Some (hval_val lookup_res2)). split => //=.
-      apply elem_of_list_fmap.
-      by exists (i, Some lookup_res2).
-    + exists (loc_host_var i, None). split => //=.
-      apply elem_of_list_fmap.
-      by exists (i, None).                                     
+  move: f.
+  induction l => //=; first by intros; apply NoDup_nil.
+  move => f HInj1. apply NoDup_cons. split.
+  - move => HContra. apply elem_of_lookup_imap in HContra.
+    destruct HContra as [i [y [Heq Helem]]].
+    by apply HInj1 in Heq.
+  - apply IHl. move => n1 n2 t1 t2 Heq.
+    simpl in Heq. apply HInj1 in Heq. lia.
+Qed.
+    
+Lemma heapg_of_list_lookup {T: Type} (l: list T) (f: N -> loc) (g: T -> heap_val) (n: N):
+  Inj eq eq f ->
+  (heap_gmap_of_list l f g) !! (f n) = option_map (fun x => Some (g x)) (l !! (N.to_nat n)).
+Proof with resolve_finmap.
+  move => HInj.
+  unfold heap_gmap_of_list, option_map.
+  remember_lookup.
+  destruct lookup_res => //=...
+  - apply HInj in H0. subst.
+    rewrite Nat2N.id. by rewrite Helem.
+  - apply HInj, N_of_nat_inj in H1. subst...
+  - apply nodup_imap_inj1.
+    move => n1 n2 t1 t2 Heq...
+    by apply HInj, N_of_nat_inj in H1.
+  - rewrite -> elem_of_list_fmap in H2.
+    remember (l !! N.to_nat n) as lookup_res eqn: Hlookup; symmetry in Hlookup.
+    destruct lookup_res => //=.
+    exfalso. apply H2.
+    exists (f n, Some (g t)). split => //=.
+    apply elem_of_lookup_imap.
+    exists (N.to_nat n), t. split => //=.
+    by rewrite N2Nat.id.
 Qed.
 
+(* Technically this is true without injectivity of f as well, but unfortunately our tactic is a 
+     bit too aggressive and makes this unprovable without injectivity. Luckily all uses in this
+     project will be injective. *)
+Lemma heapg_of_list_lookup_none {T: Type} (l: list T) (f: N -> loc) (g: T -> heap_val) (lo: loc):
+  Inj eq eq f ->
+  (forall x, f x <> lo) ->
+  (heap_gmap_of_list l f g) !! lo = None.
+Proof with resolve_finmap.
+  move => HInj Hne.
+  unfold heap_gmap_of_list.
+  remember_lookup.
+  destruct lookup_res => //=...
+  - exfalso. by apply (Hne (N.of_nat x)).
+  - apply HInj, N_of_nat_inj in H1. subst...
+  - apply nodup_imap_inj1.
+    move => n1 n2 t1 t2 Heq...
+    by apply HInj, N_of_nat_inj in H1.
+Qed.
+    
+Lemma heapg_hs_hs_lookup (hs: host_state) (i: id):
+  (heap_gmap_of_hs hs) !! (loc_host_var i) = option_map (option_map (fun y => hval_val y)) (hs !! i).
+Proof with resolve_finmap.
+  remember_lookup.
+  unfold heap_gmap_of_hs, option_map in *.
+  destruct lookup_res...
+  - by rewrite Helem.
+  - apply NoDup_fmap...
+    move => [x1 y1] [x2 y2] Heq...
+    destruct y1, y2 => //...
+  - rewrite -> elem_of_list_fmap in H2.
+    remember (hs !! i) as lookup_res2 eqn: Hlookup2; symmetry in Hlookup2.
+    repeat destruct lookup_res2 as [lookup_res2|] => //=; exfalso; apply H2...
+    + exists (loc_host_var i, Some (hval_val lookup_res2)). split => //=.
+      apply elem_of_list_fmap.
+      exists (i, Some lookup_res2). split...
+    + exists (loc_host_var i, None). split => //=.
+      apply elem_of_list_fmap.
+      exists (i, None). split...
+Qed.
+
+Lemma heapg_hs_noths_lookup (hs: host_state) (l: loc):
+  loc_typeof l <> LOC_HV ->
+  (heap_gmap_of_hs hs) !! l = None.
+Proof with resolve_finmap.
+  remember_lookup.
+  move => HLT.
+  destruct lookup_res => //=.
+  unfold heap_gmap_of_hs in Hlookup...
+  apply NoDup_fmap...
+  move => [x1 y1] [x2 y2] Heq...
+  unfold option_map in H2.
+  destruct y1, y2 => //...
+Qed.
+
+Lemma heapg_locs_locs_lookup (locs: list host_value) (n: N):
+  (gmap_of_locs locs) !! (loc_local_var n) = option_map (fun y => Some (hval_val y)) (locs !! (N.to_nat n)).
+Proof with resolve_finmap.
+  remember_lookup.
+  unfold gmap_of_locs in Hlookup.
+  rewrite -> heapg_of_list_lookup in Hlookup.
+  - unfold option_map in *. by destruct lookup_res...
+  - move => x y Heq. by inversion Heq.
+Qed.  
+
+Lemma heapg_locs_notlocs_lookup (locs: list host_value) (l: loc):
+  loc_typeof l <> LOC_LV ->
+  (gmap_of_locs locs) !! l = None.
+Proof with resolve_finmap.
+  remember_lookup.
+  move => HLT.
+  destruct lookup_res => //=.
+  unfold gmap_of_locs in Hlookup.
+  rewrite -> heapg_of_list_lookup_none in Hlookup; last by destruct l...
+  move => x y Heq; by inversion Heq.
+Qed.
+
+Lemma heapg_store_hs_lookup (s: store_record) (i: id):
+  (gmap_of_store s) !! (loc_host_var i) = None.
+Proof.
+  remember_lookup.
+  destruct lookup_res => //=.
+  unfold gmap_of_store in Hlookup.
+  unfold map_union in Hlookup.
+  repeat rewrite lookup_union_with in Hlookup.
+  unfold union_with, option_union_with in Hlookup.
+Admitted.
+  
 Definition gmap_of_state (σ : state) : gmap loc (option heap_val) :=
   let (hss, locs) := σ in
   let (hs, s) := hss in
@@ -734,7 +825,8 @@ Definition gmap_of_state (σ : state) : gmap loc (option heap_val) :=
 Lemma gmap_of_state_lookup_hs: forall hs s locs id,
     (gmap_of_state (hs, s, locs)) !! (loc_host_var id) = option_map (option_map (fun v => hval_val v)) (hs !! id).
 Proof.
-  move => hs s locs id.
+  move => hs s locs id. unfold gmap_of_state.
+  rewrite lookup_union. unfold union_with, option_union_with.
 Admitted.
 
 Lemma gmap_of_state_lookup_locs: forall hs s locs n,
